7ebf7b3e91bdc4df9a4c92e4f3c1ede470c06c64
Hawa Abdi <abdih17@hotmail.com>
2016-09-23 14:03:56 -0700

    Question 2 revision.

--- app.js
+++ app.js
@@ -26,11 +26,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var product = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
+  var output = [product, message];
+  return output;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -44,11 +47,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var product2 = a * b * c;
+  var message = 'The product of ' + a + ' and ' + b + 'and' + 'c' + ' is ' + product2 + '.';
+  var output = [product2, message];
+  return output;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
